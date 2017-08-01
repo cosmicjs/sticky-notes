90370b4356d96cf4d475c8203f7ff613535e0c8e
musaghauri <musaghauri@hotmail.com>
2017-08-01 19:07:30 +0500

    chore(webpack): added config file

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  context: __dirname,
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '*']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['react', 'es2015']
+      }
+    },
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'sass-loader'
+      //   ]
+      // }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET),
+      'process.env.COSMIC_READ_KEY': JSON.stringify(process.env.COSMIC_READ_KEY),
+      'process.env.COSMIC_WRITE_KEY': JSON.stringify(process.env.COSMIC_WRITE_KEY)
+    }),
+    new LiveReloadPlugin({appendScriptTag: true})
+  ]
+};
