d794a917f134e8b651a0d8a5373678e3c98964af
musaghauri <musaghauri@hotmail.com>
2017-08-01 18:51:15 +0500

    chore(package): added packages for server

--- server/index.js
+++ server/index.js
@@ -34,5 +34,5 @@ app.get('*', function (request, response) {
 });
 
 app.listen(PORT, function () {
-  console.log("Rockin out on port " + PORT + " homie");
+  console.log("Server running on " + PORT );
 });
