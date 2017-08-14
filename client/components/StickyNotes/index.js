74928244b15cc8ed2e8298cccc34438746eacec7
musaghauri <musaghauri@hotmail.com>
2017-08-15 01:16:09 +0500

    chore(package): added moment

--- client/components/StickyNotes/index.js
+++ client/components/StickyNotes/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { NoteListWrapper, NoteWrapper, Note } from './styles';
+import moment from 'moment/moment'
 class StickyNotes extends Component {
 
 
@@ -11,7 +12,7 @@ class StickyNotes extends Component {
           !!options && options.toArray().map((option, index) => (
             <NoteWrapper key={`note_${index}`}>
               <Note backgroundColor="#23c6c8" color="#ffffff" rotate={4}>
-                <small>{option.get('created')}</small>
+                <small>{moment(option.get('created')).fromNow()}</small>
                 <h4 onClick={() => this.props.handleClick(option)}>{option.get('title')}</h4>
                 <p>{option.get('content')}</p>
                 <a onClick={() => this.props.deleteOption(option.get('slug'), index)} className="text-danger pull-right"><i className="fa fa-trash-o"></i></a>
