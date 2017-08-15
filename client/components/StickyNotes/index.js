6ec07ae94b8d01bdb8c6ee9ad150b447bb55f9d0
Muhammad Musa <musaghauri@hotmail.com>
2017-08-15 19:47:33 +0500

    chore(home): showed certain data

--- client/components/StickyNotes/index.js
+++ client/components/StickyNotes/index.js
@@ -13,8 +13,8 @@ class StickyNotes extends Component {
             <NoteWrapper key={`note_${index}`}>
               <Note backgroundColor="black" color="#ffffff" rotate={index%2 === 0? -2 : 4}>
                 <small>{moment(option.get('created')).fromNow()}</small>
-                <h4 onClick={() => this.props.handleClick(option)}>{option.get('title')}</h4>
-                <p>{option.get('content')}</p>
+                <h4 onClick={() => this.props.handleClick(option)}>{option.get('title').substring(0,11)}</h4>
+                <p>{option.get('content').substring(0, 41)}</p>
                 <a onClick={() => this.props.deleteOption(option.toJS(), index)} className="text-danger pull-right"><i className="fa fa-trash-o"></i></a>
                 <a onClick={() => this.props.editOption(option, index)} style={{ marginRight: "1vw" }} className="text-warning pull-right"><i className="fa fa-pencil"></i></a>
               </Note>
