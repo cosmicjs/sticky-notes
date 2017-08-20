ab6db0e538c788e04348d7f56de0ea5da82ec223
Muhammad Musa <musaghauri@hotmail.com>
2017-08-20 23:07:10 +0500

    chore(stickyNote): made clickable where possible

--- client/components/StickyNotes/index.js
+++ client/components/StickyNotes/index.js
@@ -12,9 +12,11 @@ class StickyNotes extends Component {
           !!options && options.toArray().map((option, index) => {
           return  <NoteWrapper key={`note_${index}`}>
               <Note backgroundColor={option.getIn(['metadata', 'color'])||"black"} color="#ffffff" rotate={index%2 === 0? -2 : 4}>
-                <small>{moment(option.get('created')).fromNow()}</small>
-                <h4 onClick={() => this.props.handleClick(option)}>{option.get('title').substring(0,11)}</h4>
-                <p>{option.get('content').substring(0, 41)}</p>
+                <span onClick={() => this.props.handleClick(option)}>
+                  <small>{moment(option.get('created')).fromNow()}</small>
+                  <h4>{option.get('title').substring(0,11)}</h4>
+                  <p>{option.get('content').substring(0, 41)}</p>
+                </span>
                 <span className="buttons">
                   <a onClick={() => this.props.deleteOption(option.toJS(), index)} className="pull-right"><i className="fa fa-trash-o"></i></a>
                   <a onClick={() => this.props.editOption(option, index)} style={{ marginRight: "1vw" }} className="pull-right"><i className="fa fa-pencil"></i></a>
